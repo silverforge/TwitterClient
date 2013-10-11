697dd692750fd680cc5dd6dd707132892134463b
unknown <Janos_Murvai-Gaal@EPHUBUDW0079.budapest.epam.com>
2013-10-11 13:42:27 +0200

    mgj : more tests

---
+++ Silverforge.TwitterClient.Test/Converters/BoolToOpacityConverterTest.cs
@@ -0,0 +1,82 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Silverforge.TwitterClient.Converters;
+
+namespace Silverforge.TwitterClient.Test.Converters
+{
+	[TestFixture]
+	public class BoolToOpacityConverterTest
+	{
+		private readonly BoolToOpacityConverter converter = new BoolToOpacityConverter();
+
+		[Test]
+		public void Convert_True_Valid()
+		{
+			var convert = converter.Convert(true, null, null, null);
+			convert.Should().BeOfType<double>().And.Be(0.5d);
+		}
+
+		[Test]
+		public void Convert_False_Valid()
+		{
+			var convert = converter.Convert(false, null, null, null);
+			convert.Should().BeOfType<double>().And.Be(0.1d);
+		}
+
+		[Test]
+		public void Convert_Null_InValid()
+		{
+			var convert = converter.Convert(null, null, null, null);
+			convert.Should().BeOfType<double>().And.Be(0.1d);
+		}
+
+		[Test]
+		public void Convert_String_InValid()
+		{
+			var convert = converter.Convert("Honey", null, null, null);
+			convert.Should().BeOfType<double>().And.Be(0.1d);
+		}
+
+		[Test]
+		public void Convert_Int_InValid()
+		{
+			var convert = converter.Convert(1, null, null, null);
+			convert.Should().BeOfType<double>().And.Be(0.1d);
+		}
+
+		[Test]
+		public void ConvertBack_True_Valid()
+		{
+			var convert = converter.ConvertBack(0.5d, null, null, null);
+			convert.Should().BeOfType<bool>().And.Be(true);
+		}
+
+		[Test]
+		public void ConvertBack_False_Valid()
+		{
+			var convert = converter.ConvertBack(0.1d, null, null, null);
+			convert.Should().BeOfType<bool>().And.Be(false);
+		}
+
+		[Test]
+		public void ConvertBack_False_Null_InValid()
+		{
+			var convert = converter.ConvertBack(null, null, null, null);
+			convert.Should().BeOfType<bool>().And.Be(false);
+		}
+
+		[Test]
+		public void ConvertBack_False_String_InValid()
+		{
+			var convert = converter.ConvertBack("Honey", null, null, null);
+			convert.Should().BeOfType<bool>().And.Be(false);
+		}
+
+		[Test]
+		public void ConvertBack_False_Int_InValid()
+		{
+			var convert = converter.ConvertBack(5, null, null, null);
+			convert.Should().BeOfType<bool>().And.Be(false);
+		}
+	}
+}
--- Silverforge.TwitterClient/Converters/BoolToOpacityConverter.cs
+++ Silverforge.TwitterClient/Converters/BoolToOpacityConverter.cs
@@ -8,16 +8,28 @@ namespace Silverforge.TwitterClient.Converters
 	{
 		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
 		{
-			if ((bool)value)
-				return 0.5d;
+			if (value == null)
+				return 0.1d;
+
+			if (value is bool)
+			{
+				if ((bool) value)
+					return 0.5d;
+			}
 
 			return 0.1d;
 		}
 
 		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
 		{
-			if (((double) value).Equals(0.5))
-				return true;
+			if (value == null)
+				return false;
+
+			if (value is double)
+			{
+				if (((double) value).Equals(0.5))
+					return true;
+			}
 
 			return false;
 		}
